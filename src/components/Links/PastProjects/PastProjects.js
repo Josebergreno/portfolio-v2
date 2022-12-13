515bcbcda61e2ca1e0b04aec9e3bc529791a7faf
Joseph Bergren <bergren.bergren@yahoo.com>
2022-12-13 16:50:06 -0700

    formatted the past projects page

--- src/components/Links/PastProjects/PastProjects.js
+++ src/components/Links/PastProjects/PastProjects.js
@@ -1,18 +1,30 @@
 import styles from "./PastProjects.module.css";
+import snakeGameThumbnail from "../../UI/Icons/newSnakeGameThumbnail.jpg";
 
 const PastProjects = (props) => {
   return (
-    <>
-      <a
-        href="google.com"
-        onClick={props.onClick}
-        className={styles["container"]}
-      >
-        <div className={styles["past-projects--container"]}>
-          <div className={styles["text-holder"]}>Past projects</div>
-        </div>
-      </a>
-    </>
+    <a
+      href="google.com"
+      onClick={props.onClick}
+      className={styles["container"]}
+    >
+      <div className={styles["header-container"]}>
+        <div className={styles["text-holder"]}>Past projects</div>
+      </div>
+
+      <div className={styles["project-body"]}>
+        <button className={styles["button"]}>back</button>
+        <a href="https://sn8ke-game.netlify.app/" className={styles["link"]}>
+          <img
+            className={styles["snake-game--thumbnail"]}
+            alt="snake-game"
+            src={snakeGameThumbnail}
+          ></img>
+          Snake Game
+        </a>
+        <button className={styles["button"]}>next</button>
+      </div>
+    </a>
   );
 };
 
