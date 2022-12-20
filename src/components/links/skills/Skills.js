d0b78dbfeb1254b8453a223b108ab15d587dd1ee
Joseph Bergren <bergren.bergren@yahoo.com>
2022-12-19 17:51:02 -0700

    added descriptions to skill section

--- src/components/links/skills/Skills.js
+++ src/components/links/skills/Skills.js
@@ -48,15 +48,28 @@ const SkillsLink = (props) => {
             firstRowInView ? styles["row-container"] : styles["invisible"]
           }
         >
-          <div className={styles["indiv-icon--container"]}>
-            <img
-              alt={"react-icon"}
-              className={styles["icon"]}
-              src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-            />
-            <span className={styles["under-title"]}>React</span>
+          <div
+            onClick={clickHandler}
+            className={styles["indiv-icon--container"]}
+          >
+            {clicked !== "React" ? (
+              <>
+                <img
+                  alt={"react-icon"}
+                  className={styles["icon"]}
+                  src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+                />
+                <span className={styles["under-title"]}>React</span>
+              </>
+            ) : (
+              <div className={styles["description"]}>
+                React builds interactive user interfaces and web applications
+                quickly and efficiently with significantly less code than you
+                would with vanilla JavaScript.
+              </div>
+            )}
           </div>
-
+          ;
           <div
             onClick={clickHandler}
             className={styles["indiv-icon--container"]}
@@ -77,14 +90,26 @@ const SkillsLink = (props) => {
               </div>
             )}
           </div>
-
-          <div className={styles["indiv-icon--container"]}>
-            <img
-              alt={"typescript-icon"}
-              className={styles["icon"]}
-              src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"
-            />
-            <span className={styles["under-title"]}>Typescript</span>
+          <div
+            onClick={clickHandler}
+            className={styles["indiv-icon--container"]}
+          >
+            {clicked !== "Typescript" ? (
+              <>
+                <img
+                  alt={"typescript-icon"}
+                  className={styles["icon"]}
+                  src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg"
+                />
+                <span className={styles["under-title"]}>Typescript</span>
+              </>
+            ) : (
+              <div className={styles["description"]}>
+                TypeScript extends JavaScript and improves the developer
+                experience. It enables developers to add type safety to their
+                projects
+              </div>
+            )}
           </div>
         </div>
         <div
@@ -93,29 +118,66 @@ const SkillsLink = (props) => {
             secondRowInView ? styles["row-container"] : styles["invisible"]
           }
         >
-          <div className={styles["indiv-icon--container"]}>
-            <img
-              alt={"javascript-icon"}
-              className={styles["icon"]}
-              src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
-            />
-            <span className={styles["under-title"]}>Javascript</span>
+          <div
+            onClick={clickHandler}
+            className={styles["indiv-icon--container"]}
+          >
+            {clicked !== "Javascript" ? (
+              <>
+                <img
+                  alt={"javascript-icon"}
+                  className={styles["icon"]}
+                  src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
+                />
+                <span className={styles["under-title"]}>Javascript</span>
+              </>
+            ) : (
+              <div className={styles["description"]}>
+                Javascript is used by programmers across the world to create
+                dynamic and interactive web content like applications and
+                browsers.
+              </div>
+            )}
           </div>
-          <div className={styles["indiv-icon--container"]}>
-            <img
-              alt={"css-icon"}
-              className={styles["icon"]}
-              src="https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg"
-            />
-            <span className={styles["under-title"]}>CSS</span>
+          <div
+            onClick={clickHandler}
+            className={styles["indiv-icon--container"]}
+          >
+            {clicked !== "CSS" ? (
+              <>
+                <img
+                  alt={"css-icon"}
+                  className={styles["icon"]}
+                  src="https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg"
+                />
+                <span className={styles["under-title"]}>CSS</span>
+              </>
+            ) : (
+              <div className={styles["description"]}>
+                CSS is the language we use to style an HTML document. CSS
+                describes how HTML elements should be displayed.
+              </div>
+            )}
           </div>
-          <div className={styles["indiv-icon--container"]}>
-            <img
-              alt={"html-icon"}
-              className={styles["icon"]}
-              src="https://upload.wikimedia.org/wikipedia/commons/8/82/Devicon-html5-plain.svg"
-            />
-            <span className={styles["under-title"]}>HTML</span>
+          <div
+            onClick={clickHandler}
+            className={styles["indiv-icon--container"]}
+          >
+            {clicked !== "HTML" ? (
+              <>
+                <img
+                  alt={"html-icon"}
+                  className={styles["icon"]}
+                  src="https://upload.wikimedia.org/wikipedia/commons/8/82/Devicon-html5-plain.svg"
+                />
+                <span className={styles["under-title"]}>HTML</span>
+              </>
+            ) : (
+              <div className={styles["description"]}>
+                HTML is the standard markup language for Web pages. With HTML
+                you can create your own Website.
+              </div>
+            )}
           </div>
         </div>
         <div
@@ -124,29 +186,68 @@ const SkillsLink = (props) => {
             thirdRowInView ? styles["row-container"] : styles["invisible"]
           }
         >
-          <div className={styles["indiv-icon--container"]}>
-            <img
-              alt={"responsive-icon"}
-              className={styles["icon"]}
-              src={responsiveIcon}
-            />
-            <span className={styles["under-title"]}>Responsive Design</span>
+          <div
+            onClick={clickHandler}
+            className={styles["indiv-icon--container"]}
+          >
+            {clicked !== "Responsive Design" ? (
+              <>
+                <img
+                  alt={"responsive-icon"}
+                  className={styles["icon"]}
+                  src={responsiveIcon}
+                />
+                <span className={styles["under-title"]}>Responsive Design</span>
+              </>
+            ) : (
+              <div className={styles["description"]}>
+                Responsive Design is a development approach that dynamic changes
+                the appearance of a website, depending on the screen size and
+                orientation of the device being used to view it.
+              </div>
+            )}
           </div>
-          <div className={styles["indiv-icon--container"]}>
-            <img
-              alt={"git-icon"}
-              className={styles["git-icon"]}
-              src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg"
-            />
-            <span className={styles["under-title"]}>git</span>
+          <div
+            onClick={clickHandler}
+            className={styles["indiv-icon--container"]}
+          >
+            {clicked !== "git" ? (
+              <>
+                <img
+                  alt={"git-icon"}
+                  className={styles["git-icon"]}
+                  src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg"
+                />
+                <span className={styles["under-title"]}>git</span>
+              </>
+            ) : (
+              <div className={styles["description"]}>
+                Git is a free and open source distributed version control system
+                designed to handle everything from small to very large projects
+                with speed and efficiency.
+              </div>
+            )}
           </div>
-          <div className={styles["indiv-icon--container"]}>
-            <img
-              alt={"github-icon"}
-              className={styles["icon"]}
-              src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
-            />
-            <span className={styles["under-title"]}>Github</span>
+          <div
+            onClick={clickHandler}
+            className={styles["indiv-icon--container"]}
+          >
+            {clicked !== "Github" ? (
+              <>
+                <img
+                  alt={"github-icon"}
+                  className={styles["icon"]}
+                  src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+                />
+                <span className={styles["under-title"]}>Github</span>
+              </>
+            ) : (
+              <div className={styles["description"]}>
+                GitHub is a code hosting platform for version control and
+                collaboration. It lets you and others work together on projects
+                from anywhere.
+              </div>
+            )}
           </div>
         </div>
       </div>
